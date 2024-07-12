012f12018e7ec57bf80990f694069ced2063bc99
AmaN <webdevaman2@gmail.com>
2024-07-12 13:16:18 +0530

    added auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,16 @@
-import { useContext, useState } from 'react';
-import { RxCross2, RxHamburgerMenu } from 'react-icons/rx';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { useContext, useState } from "react";
+import { RxCross2, RxHamburgerMenu } from "react-icons/rx";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
-import { Avatar, Modal } from '@mui/material';
-import { logout } from '../config/firebase';
-import { useUser } from '../Context/User';
-import Auth from './auth/Auth';
-import logo from './logo.png';
-import { EditorContext } from './PracticeJS';
-import { LoginModal } from './LoginModal';
+import { Avatar, Modal } from "@mui/material";
+import { logout } from "../config/firebase";
+import { useUser } from "../Context/User";
+import Auth from "./auth/Auth";
+import logo from "./logo.png";
+import { EditorContext } from "./PracticeJS";
+import { LoginModal } from "./LoginModal";
 
 export default function Header() {
-
   const [isOpen, setIsOpen] = useState(false);
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
@@ -23,10 +22,9 @@ export default function Header() {
     setIsOpen(!isOpen);
   };
 
-  console.log('user', user)
+  console.log("user", user);
 
   return (
-    
     <nav className="bg-[#070F2B] shadow-lg p-3 px-8 ">
       <Modal
         open={open}
@@ -41,15 +39,14 @@ export default function Header() {
 
       <div className="flex justify-between items-center max-w-7xl mx-auto px-1 ">
         {/* Left side logo */}
-        
+
         <div className="flex-shrink-0 flex items-center justify-start">
-        <img className='h-6 w-full' src={logo} alt="Logo" />;
+          <img className="h-6 w-full" src={logo} alt="Logo" />;
           {/* <Typography variant="h6" noWrap component="div" className='text-white'>
             JS_INFINITY_LAB
           </Typography> */}
-          
         </div>
-        
+
         {/* Center links */}
         <div className="hidden md:block">
           <div className="ml-3 flex flex-row-reverse justify-between items-baseline space-x-4">
@@ -62,8 +59,9 @@ export default function Header() {
                   <NavLink
                     to="/"
                     className={({ isActive }) =>
-                      `${isActive ? 'text-white' : 'text-grey-700'} text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
-
+                      `${
+                        isActive ? "text-white" : "text-grey-700"
+                      } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
                     }
                   >
                     Home
@@ -72,57 +70,63 @@ export default function Header() {
                 <li
                   onClick={() => {
                     if (user?.userDetail && user.currentFile) {
-                      navigate(`/playground/${user?.currentFile?.id}`)
+                      navigate(`/playground/${user?.currentFile?.id}`);
                     } else {
-                      navigate('/playground')
+                      navigate("/playground");
                     }
                   }}
                 >
                   <NavLink
                     className={({ isActive }) =>
-                      `${isActive ? "text-white" : "text-grey-700"} text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
+                      `${
+                        isActive ? "text-white" : "text-grey-700"
+                      } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
                     }
                   >
                     Playground
-                  </NavLink >
+                  </NavLink>
                 </li>
                 <li>
                   <NavLink
-                    to='/notes'
+                    to="/notes"
                     className={({ isActive }) =>
-                      `${isActive ? "text-white" : "text-grey-700"} text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
+                      `${
+                        isActive ? "text-white" : "text-grey-700"
+                      } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
                     }
                   >
                     Notes
-                  </NavLink >
+                  </NavLink>
                 </li>
                 <li>
                   <NavLink
-                    to='/test-skill'
+                    to="/test-skill"
                     className={({ isActive }) =>
-                      `${isActive ? "text-white" : "text-grey-700"} text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
+                      `${
+                        isActive ? "text-white" : "text-grey-700"
+                      } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`
                     }
                   >
                     Test skills
-                  </NavLink >
+                  </NavLink>
                 </li>
               </ul>
             </div>
-
           </div>
         </div>
         {/* ? */}
 
-
         {/* Right side login/signup and hamburger menu */}
         <div className="-mr-2 flex items-center md:hidden">
           <button onClick={toggleMenu}>
-            {!isOpen ? <RxHamburgerMenu className='text-2xl text-white' />
-              : <RxCross2 className='text-2xl text-white' />}
+            {!isOpen ? (
+              <RxHamburgerMenu className="text-2xl text-white" />
+            ) : (
+              <RxCross2 className="text-2xl text-white" />
+            )}
           </button>
-
         </div>
-        <div className='hidden md:flex space-x-3'>
+        <div className="hidden md:flex md:items-center space-x-3">
           {/* <Link
             to="#"
             onClick={() => {
@@ -133,48 +137,52 @@ export default function Header() {
             {user?.userDetail ? <Avatar sx={{ width: 28, height: 28 }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
               : 'Log in'}
           </Link> */}
+          {user?.userDetail ? (
+            <Avatar
+              sx={{ width: 28, height: 28 }}
+              alt={user?.userDetail?.displayName}
+              src={`${user.userDetail.photoURL}`}
+            />
+          ) : (
+            <>
               <LoginModal type="login" />
               <LoginModal type="" />
-          
+            </>
+          )}
 
-          {
-            user?.userDetail && (
-              <button className="logout">
-                <Link
-                  to="#"
-                  onClick={() => {
-                    logout();
-                    setUser(null);
-                    setHtml('');
-                    setCss('');
-                    setJs('');
-                    navigate('/')
-                  }}
-                  className="inline-flex items-center justify-center px-3 py-2 font-medium text-slim rounded-md text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
-                >
-                  Logout
-                </Link>
-              </button>
-            )
-          }
-
+          {user?.userDetail && (
+            <button className="logout">
+              <Link
+                to="#"
+                onClick={() => {
+                  logout();
+                  setUser(null);
+                  setHtml("");
+                  setCss("");
+                  setJs("");
+                  navigate("/");
+                }}
+                className="inline-flex items-center justify-center px-3 py-2 font-medium text-slim rounded-md text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+              >
+                Logout
+              </Link>
+            </button>
+          )}
         </div>
       </div>
-
-
 
       {/* Mobile menu */}
       {isOpen && (
         <div className="md:hidden">
-
           <div className="px-2 pt-2 pb-3 sm:px-3">
             <ul className="px-2 pt-2 pb-3 sm:px-3">
               <li>
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
-                    `${isActive ? 'text-orange-700' : 'text-grey-700'} block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium`
-
+                    `${
+                      isActive ? "text-orange-700" : "text-grey-700"
+                    } block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium`
                   }
                 >
                   Home
@@ -182,9 +190,11 @@ export default function Header() {
               </li>
               <li>
                 <NavLink
-                  to='/notes'
+                  to="/notes"
                   className={({ isActive }) =>
-                    `${isActive ? "text-orange-700" : "text-grey-700"} block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium`
+                    `${
+                      isActive ? "text-orange-700" : "text-grey-700"
+                    } block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium`
                   }
                 >
                   Notes
@@ -192,9 +202,11 @@ export default function Header() {
               </li>
               <li>
                 <NavLink
-                  to='/test-skill'
+                  to="/test-skill"
                   className={({ isActive }) =>
-                    `${isActive ? "text-orange-700" : "text-grey-700"} block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium`
+                    `${
+                      isActive ? "text-orange-700" : "text-grey-700"
+                    } block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium`
                   }
                 >
                   Test Skills
@@ -208,8 +220,8 @@ export default function Header() {
                 >
                   Log in
                 </Link> */}
-                 <LoginModal type="login" styles={'pt-2 px-3'} />
-                 <LoginModal type="" styles={'pt-4 px-3'} />
+                <LoginModal type="login" styles={"pt-2 px-3"} />
+                <LoginModal type="" styles={"pt-4 px-3"} />
               </li>
             </ul>
           </div>
@@ -217,11 +229,4 @@ export default function Header() {
       )}
     </nav>
   );
-
-
-
-
 }
-
-
-
--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -8,18 +8,43 @@ import {
 } from "./ui/animated-modal";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { useRef } from "react";
+import { logInWithEmailAndPassword, registerWithEmailAndPassword, signInWithGoogle } from "../config/firebase";
 
 export function LoginModal({ type,styles }) {
-  const handleSubmit = (e) => {
-    if(type=="login") {
+  const firstnameRef = useRef(null);
+  const lastnameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if(type=="login") {
+      if(emailRef.current.value === "" || passwordRef.current.value === "") {
+        console.log("empty fields");
+      }else{
+        await logInWithEmailAndPassword(
+          emailRef.current.value,
+          passwordRef.current.value
+        )
+      }
     }else{
-        e.preventDefault();
-        console.log("sign up");
         // get form data
         const formData = new FormData(e.target);
-        const data = Object.fromEntries(formData.entries());
-        console.log(data);
+        const firstname = formData.get("firstname");
+        const lastname = formData.get("lastname");
+        const email = formData.get("email");
+        const password = formData.get("password");
+        console.log(firstname,lastname,email,password);
+        const resp = await registerWithEmailAndPassword(
+          firstname,
+          lastname,
+          emailRef.current.value,
+          passwordRef.current.value
+        );
+         
+        console.log(resp);
     }
 
   };
@@ -43,36 +68,42 @@ export function LoginModal({ type,styles }) {
               </span>{" "}
               ✈️
             </h4>
-            <form className="my-8" onSubmit={handleSubmit}>
+            <form action=""
+                onSubmit={handleSubmit}
+          
+            >
+
               {type !=="login" && <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
                 <LabelInputContainer>
                   <Label htmlFor="firstname">First name</Label>
-                  <Input id="firstname" placeholder="Tyler" type="text" />
+                  <Input ref={firstnameRef} name="firstname" id="firstname" placeholder="Tyler" type="text" />
                 </LabelInputContainer>
                 <LabelInputContainer>
                   <Label htmlFor="lastname">Last name</Label>
-                  <Input id="lastname" placeholder="Durden" type="text" />
+                  <Input ref={lastnameRef} id="lastname" name="lastname" placeholder="Durden" type="text" />
                 </LabelInputContainer>
               </div>}
               <LabelInputContainer className="mb-4">
                 <Label htmlFor="email">Email Address</Label>
                 <Input
+                ref={emailRef}
                   id="email"
                   placeholder="projectmayhem@fc.com"
                   type="email"
+                  name="email"
                 />
               </LabelInputContainer>
               <LabelInputContainer className="mb-4">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" placeholder="••••••••" type="password" />
+                <Input ref={passwordRef} id="password" name="password" placeholder="••••••••" type="password" />
               </LabelInputContainer>
               <button
                 className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-                type="submit"
               >
                 {type==="login" ? "Login": "Sign Up"} &rarr;
                 <BottomGradient />
               </button>
+              </form>
 
               <div className="flex flex-col pt-5">
                 {/* <button
@@ -87,7 +118,12 @@ export function LoginModal({ type,styles }) {
                 </button> */}
                 <button
                   className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-                  type="submit"
+                  onClick={()=>{
+                    signInWithGoogle().then((authUser)=>{
+                      // close modal
+
+                    })
+                  }}
                 >
                   <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                   <span className="text-neutral-700 dark:text-neutral-300 text-sm">
@@ -96,7 +132,6 @@ export function LoginModal({ type,styles }) {
                   <BottomGradient />
                 </button>
               </div>
-            </form>
 
             {/* <div className="flex justify-center items-center">
               {images.map((image, idx) => (
